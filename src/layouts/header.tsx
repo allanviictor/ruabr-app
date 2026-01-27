82e37975fac38271501004b7c98e600e0b0fef3e
allan victor de moura <allanviictor07@gmail.com>
2026-01-26 22:52:13 -0300

    exclusão de variavies

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,31 +1,3 @@
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
-
-const navLinks = [
-  { href: "/#informe-se", label: "Informe-se" },
-  { href: "/#como-ajudar", label: "Como ajudar" },
-  { href: "/#noticias", label: "Notícias e histórias" },
-  { href: "/#sobre-nos", label: "Sobre nós" },
-];
-
-const navigationMenuItems = [
-  { title: "Home", href: "#" },
-  { title: "Blog", href: "#blog" },
-  { title: "Docs", href: "#docs" },
-];
-
 export function Header() {
-  return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem key={"aln"}>
-          <NavigationMenuLink asChild>aln</NavigationMenuLink>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-  );
+  return "";
 }
